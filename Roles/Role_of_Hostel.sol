07926c51894a1ec32df0423fac58372a69ad97b6
Adam Shettima Musti <85008362+adammusti@users.noreply.github.com>
2022-03-23 21:13:24 +0530

    Create Role_of_Hostel.sol

---
+++ Roles/Role_of_Hostel.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract HostelWarden {
+    function checkes(uint Clear_Candidate) public pure returns (bool warden){
+        
+        if  (Clear_Candidate == 0) {
+            return true;
+        }
+
+        else if  (Clear_Candidate != 0) {
+            return false;
+            }
+            
+            
+        }
+        
+    }
+    
